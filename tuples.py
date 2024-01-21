1734107349dbbe72bb9f9ed11382a025a6ee3c6c
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:11:07 +0600

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# task1
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+# task2
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+# task3
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+# task4
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
