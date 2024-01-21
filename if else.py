0542b027def288d1ba9bdf767a48a6a6427f86ba
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:10:53 +0600

    Create if else.py

---
+++ if else.py
@@ -0,0 +1,54 @@
+# task1
+a = 50
+b = 10
+if a >b:
+     print("Hello World")
+
+# task2
+a = 50
+b = 10
+if a !=b:
+    print("Hello World")
+
+# task3
+a = 50
+b = 10
+if a ==b:
+  print("Yes")
+else:
+  print("No")
+
+# task4
+a = 50
+b = 10
+if a ==b:
+  print("1")
+elif a >b:
+  print("2")
+else:
+  print("3")
+
+# task5
+if a == b and c == d:
+  print("Hello")
+
+
+# task6
+if a == b or c == d:
+  print("Hello")
+
+# task7
+  if 5 > 2:
+    print("YES")
+
+# task8
+a = 2
+b = 5
+print("YES")  if a == b else  print("NO")
+
+# task9
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+  print("YES")
