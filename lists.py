b238c191434ef871cb0b9b5ef9b22bd75bfe9580
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:10:57 +0600

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,32 @@
+# task1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+# task2
+fruits = ["apple", "banana", "cherry"]
+fruits[0]="kiwi"
+
+# task3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+# task4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+# task5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+# task6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+# task7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+# task8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
