afc9a208dec610ecf9522b8903b710d0be175c31
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:10:43 +0600

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,40 @@
+# task1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+
+# task2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"]=2020
+
+# task3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"]="red"
+
+# task4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+# task5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
