afd9fee87d7dcf434102b36b2d25184cad68adde
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:10:59 +0600

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,19 @@
+#task1
+print(10* 5)
+
+#task2
+print(10/2)
+
+
+#task3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#task4
+
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+#task5
+  if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
