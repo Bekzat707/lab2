8cc888d154a10a7823b2d51058126143811d8c6c
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:10:48 +0600

    Create For loops.py

---
+++ For loops.py
@@ -0,0 +1,23 @@
+# task1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits :
+  print(x)
+
+# task2
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+# task3
+  for x in range(6):
+    print(x)
+
+# task4
+    
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+print(x)
