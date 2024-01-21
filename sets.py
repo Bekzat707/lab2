18ac256a22171cc7a6e5230820bcf3569697de49
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 17:11:04 +0600

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,21 @@
+# task1
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+  
+# task2
+  fruits = {"apple", "banana", "cherry"}
+  fruits.add("orange")
+
+# task3
+  fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+# task4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+# task5
+fruits = {"apple", "banana", "cherry"}  
+fruits.discard("banana")
